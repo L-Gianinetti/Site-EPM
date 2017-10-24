a8413f2cad94d5fd532418a5d726f0e0ed2d8bca
L-Gianinetti <cpnv.lucas.gianinetti@gmail.com>
2017-10-24 15:40:58 +0200

    Add files via upload

--- modele/modele.php
+++ modele/modele.php
@@ -8,7 +8,7 @@
 // connexion au serveur MySQL et à la BD
 // sortie : $connexion
 function getBD() {
-    $connexion = new PDO('mysql:host=localhost;dbname=cpm ;charset=utf8', 'root', '');
+    $connexion = new PDO('mysql:host=localhost;dbname=epm;charset=utf8', 'root', '');
 // permet d'avoir plus de détails sur les erreurs retournées
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $connexion;
@@ -25,5 +25,41 @@ function getPwdFromLogin($login)
         return $donnees['pwd'];
     } else {
         return '';
+    }
+}
+
+function getTypeRecette()
+{
+    $connexion = getBD();
+    $requete = "SELECT nom FROM categoriePlat";
+    $resultats = $connexion->query($requete);
+    return $resultats;
+}
+
+function getRecette($titre, $type)
+{
+    $connexion = getBD();
+    $requete = "SELECT recette.Titre, recette.idCheminDonnee, categorieplat.Nom as categorieplatNom from recette inner join categorieplat on recette.fkCategoriePlat  = categorieplat.idCategoriePlat  where recette.Titre like '" .$titre . "%'  and categorieplat.Nom = '" . $type ."'" ;
+    $resultats = $connexion->query($requete);
+    return $resultats;
+}
+
+function getRepStockage()
+{
+    $connexion = getBD();
+    $requete = "SELECT NomRep from repprincipal";
+    $resultat = $connexion->query($requete);
+    foreach ($resultat as $row) {
+        return $row['NomRep'];
+    }
+    //return "";
+}
+function getCheminDonnee()
+{
+    $connexion = getBD();
+    $requete = "SELECT Path from chemindonnee";
+    $resultat = $connexion->query($requete);
+    foreach ($resultat as $row){
+        return $row['Path'];
     }
 }
