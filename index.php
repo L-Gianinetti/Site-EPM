9f2f1f030e66fd88bb544570e5416c3edc5b3b8f
VojislaVojo <31890358+VojislaVojo@users.noreply.github.com>
2017-10-31 09:10:28 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
-
-session_set_cookie_params(1200);
-session_start();
-
-require  'controleur/controleur.php';
-
+require 'controleur/controleur.php';
 try
 {
     if (isset($_GET['action']))
     {
         $action = $_GET['action'];
-
         switch ($action)
         {
             case 'welcome' :
@@ -19,33 +13,17 @@ try
             case 'login' :
                 login(); //appel de la fonction dans le controleur
                 break;
-            case 'recette' :
-                recette();
+            case 'photo' :
+                photo(); //appel de la fonction dans le controleur
                 break;
-            case 'ouvrir_recette' :
-                $nomfichier = $_GET['fichier'];
-                ouvrirFichierRecettes($nomfichier);
-            case 'logout' :
-                logout();
-                break;
-            case 'changer_pwd':
-                resetPwd();
-                break;
-            case 'afficher_contenu_pedagogique':
-                contenuPedagogique();
-                break;
-            case 'ouvrir_contenu_pedagogique':
-                $nomfichier = $_GET['fichier'];
-                ouvrirFichierContenuPedagogique($nomfichier); //appel de la fonction dans le controleur
-                break;
+            case 'ajoutPhoto':
+                ajoutPhoto(); //appel de la fonction dans le controleur
             default :
                 throw new Exception("action non valide");
-
         }
     }
     else
-        accueil();  // Si aucune action n’est envoyée dans l’URL
-
+        accueil(); // Si aucune action n’est envoyée dans l’URL
 }
 catch (Exception $e)
 {
--- modele/modele.php
+++ modele/modele.php
@@ -1,21 +1,17 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Melvyn.HERZIG
+ * User: Lucas.GIANINETTI
  * Date: 05.09.2017
- * Time: 14:02
+ * Time: 14:01
  */
-
 // connexion au serveur MySQL et à la BD
 // sortie : $connexion
 function getBD() {
-    $connexion = new PDO('mysql:host=localhost;dbname=epm3;charset=utf8', 'root', '');
-
+    $connexion = new PDO('mysql:host=localhost;dbname=epm;charset=utf8', 'root', '');
 // permet d'avoir plus de détails sur les erreurs retournées
-
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $connexion;
-
 }
 
 //Fonction : vérifie le login de l'utilisateur
@@ -23,71 +19,33 @@ function getBD() {
 function getPwdFromLogin($login)
 {
     $connexion = getBD();
-    $requete = "SELECT Identifiant, MotDePasse FROM Login WHERE Identifiant='" . $login . "'";
+    $requete = "SELECT idUser, pwd FROM Rights WHERE login='" . $login . "'";
     $resultats = $connexion->query($requete);
     if ($donnees = $resultats->fetch()) {
-        return $donnees['MotDePasse'];
+        return $donnees['pwd'];
     } else {
         return '';
     }
 }
 
-
-function getTypeRecette()
-{
+//Fonction: recherche les photos selon la categorie
+//Sortie :  résultat de la requête
+function getPhoto($categorie){
     $connexion = getBD();
-    $requete = "SELECT nom FROM categoriePlat";
-}
-
-//insetion du nouveau hachage pour un mot de passe
-function insertNewPwd($pwdNew,$login)
-{
-    $connexion = getBD();
-    $hachage = password_hash($pwdNew, PASSWORD_DEFAULT);
-    $requete = "UPDATE Login SET MotDePasse='".$hachage."' WHERE Identifiant ='".$login."'";
-    $connexion->query($requete);
-}
-
-//Retourne la catégorie des contenus pédagogiques
-function getTypeContenuPedagogique()
-{
-    $connexion = getBD();
-    $requete = "SELECT Nom FROM CategorieContenuPedagogique";
+    if($categorie == 'tout'){
+        $requete = "SELECT Nom, fkCategoriePhoto FROM photo";
+    }else{
+            if($categorie == 'Annee2' || $categorie == 'tout2'){
+                $requete = "SELECT Nom, fkCategoriePhoto FROM photo WHERE fkCategoriePhoto = 1 OR fkCategoriePhoto = 4";
+            }else{
+                if($categorie == 'Annee3' || $categorie == 'tout3'){
+                    $requete = "SELECT Nom, fkCategoriePhoto FROM photo WHERE fkCategoriePhoto = 5 OR fkCategoriePhoto = 6 OR fkCategoriePhoto = 7";
+                }else{
+                    $requete = "SELECT Nom, fkCategoriePhoto FROM photo WHERE fkCategoriePhoto='" . $categorie . "'";
+                }
+            }
+    }
     $resultats = $connexion->query($requete);
     return $resultats;
-}
 
-
-function getRecette($titre, $type)
-{
-    $connexion = getBD();
-    $requete = "SELECT recette.Titre, recette.idCheminDonnee, categorieplat.Nom as categorieplatNom from recette inner join categorieplat on recette.fkCategoriePlat  = categorieplat.idCategoriePlat  where recette.Titre like '" .$titre . "%'  and categorieplat.Nom = '" . $type ."'" ;
-}
-
-function getContenuPedagogique($annee, $type)
-{
-    $connexion = getBD();
-    $requete = "SELECT CheminDonnee.Path as chemindonnee, ContenuPedagogique.Nom AS typenom FROM ContenuPedagogique INNER JOIN CategorieContenuPedagogique ON ContenuPedagogique.fkCategorieContenuPedagogique = CategorieContenuPedagogique.idCategorieContenuPedagogique INNER JOIN Annee ON ContenuPedagogique.fkAnnee = Annee.idAnnee INNER JOIN CheminDonnee ON ContenuPedagogique.fkCheminDonnee = CheminDonnee.idCheminDonnee INNER JOIN RepPrincipal ON CheminDonnee.fkRepPrincipal = RepPrincipal.idRepPrincipal WHERE CategorieContenuPedagogique.Nom = '".$type."' AND Annee.Annee =".$annee;
-    $resultats = $connexion->query($requete);
-    return $resultats;
-}
-
-function getCheminDonnee()
-{
-    $connexion = getBD();
-    $requete = "SELECT Path from chemindonnee";
-    $resultat = $connexion->query($requete);
-    foreach ($resultat as $row){
-        return $row['Path'];
-    }
-}
-
-function getRepStockage() {
-    $connexion = getBD();
-    $requete = "SELECT NomRep FROM RepPrincipal";
-    $resultats = $connexion->query($requete);
-    foreach  ($resultats as $row) {
-        return $row['NomRep'];
-    }
-    return "";
 }
